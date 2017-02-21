f480242f6efbcd8a5f8c651cc5312008353a8464
allmanaj <aa828@kent.ac.uk>
2017-02-21 17:05:18 +0200

    Creted first models
    Created models for post, and comment, as well as creating initial date from the shell.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,27 @@
+import datetime
+
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=200)
+    post_body = models.TextField()
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.post_title
+
+    def was_publised_recently(self):
+        return self.pud_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    comment_body = models.TextField()
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.comment_body

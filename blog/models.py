3da1c12e3fc496b90646d7ed1ad745435aa31693
allmanaj <aa828@kent.ac.uk>
2017-02-21 17:51:51 +0200

    edited views, and introduced template system
    just what the title says

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,5 @@
 import datetime
 
-from __future__ import unicode_literals
-
 from django.db import models
 from django.utils import timezone
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 # Create your views here.
-def index(request):
-    return HttpResponse("This is the index page!")
+def detail(request, post_id):
+    return HttpResponse("This is the page where you can view the post! this post is #%s" % post_id)
+
+def feed(request):
+    latest_post_list = Post.objects.order_by('-pub_date')[:5]
+    context = {'latest_post_list': latest_post_list}
+    return render(request, 'blog/feed.html', context)
+
